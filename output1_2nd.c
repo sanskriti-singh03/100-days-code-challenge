06c1f7316dead15eafddac9ab43853537f061f24
sanskriti-singh03 <Sanskriti.25603@stu.upes.ac.in>
2025-09-03 22:05:08 +0530

    Add files via upload

---
+++ output1_2nd.c
@@ -0,0 +1,13 @@
+enter first number:10 
+enter second number:2
+sum of 10 and 2 is 12
+difference of 10 and 2 is 8
+product of 10 and 2 is 20
+quotient of 10 and 2 is 5
+
+enter first number:7
+enter second number:3
+sum of 7 and 3 is 10
+difference of 7 and 3 is 4
+product of 7 and 3 is 21
+quotient of 7 and 3 is 2
