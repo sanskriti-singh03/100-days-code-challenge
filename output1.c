bd21306a340fb8869f4b2060ea4af6b6943ebd0c
Sanskriti Singh <sanskritisingh7479@gmail.com>
2025-09-03 16:55:29 +0530

    Add files via upload

---
+++ output1.c
@@ -0,0 +1,7 @@
+enter first number:3
+enter second number:4
+sum of 3 and 4 is 7                                                                                                                               
+   
+enter first number:-1
+enter second number:20
+sum of -1 and 20 is 19
