4f43d45f3ec00bfa55179d9a0cc4c7f7b01f4832
sanskriti-singh03 <Sanskriti.25603@stu.upes.ac.in>
2025-09-10 18:12:06 +0530

    Add files via upload

---
+++ output_21_2.c
@@ -0,0 +1,5 @@
+Enter a number: 6
+6 is a perfect number.
+
+Enter a number: 10
+10 is not a perfect number.
---
+++ output_day21_1.c
@@ -0,0 +1,5 @@
+Enter a number: 1234
+Number after swapping first and last digit: 4231
+
+Enter a number: 1001
+Number after swapping first and last digit: 1001
