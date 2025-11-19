20a1e6b38574f2d1e8d5cba6f81ace781cb733c9
Sanskriti Singh <sanskritisingh7479@gmail.com>
2025-11-19 16:47:13 +0530

    Add files via upload

---
+++ day23_1.c
@@ -0,0 +1,15 @@
+//Write a program to print the following pattern:
+#include <stdio.h>
+int main() 
+{
+    int i, j, rows = 5, cols = 5;
+    for (i = 0; i < rows; i++) 
+    {
+        for (j = 0; j < cols; j++) 
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
