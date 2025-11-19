e20f1446ed8bb1f3323290dfc1b94344eb2cf598
sanskriti singh <Sanskriti.25603@stu.upes.ac.in>
2025-11-19 16:47:13 +0530

    Add files via upload

---
+++ day22_2.c
@@ -0,0 +1,18 @@
+//Write a program to find the sum of the series: 1 + 3/4 + 5/6 + 7/8 + … up to n terms.
+#include <stdio.h>
+int main() {
+    int n, i;
+    float sum = 0.0;
+
+    printf("Enter the number of terms: ");
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++) {
+        int numerator = 2 * i - 1;   
+        int denominator = 2 * i;
+        sum += (float)numerator / denominator;
+    }
+    printf("Sum of the series up to %d terms is: %.2f\n", n, sum);
+    return 0;
+}
+
---
+++ output_22_2.c
@@ -0,0 +1,2 @@
+Enter the number of terms: 4
+Sum of the series up to 4 terms is: 2.96
